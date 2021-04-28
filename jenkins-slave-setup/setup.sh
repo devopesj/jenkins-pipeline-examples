09a48d8ffc28d6794bb9e71b07c67d777fac0fbb
devopesj <ope.junaid@gmail.com>
2021-04-28 20:35:34 +0100

    jenkins jar

--- jenkins-slave-setup/setup.sh
+++ jenkins-slave-setup/setup.sh
@@ -20,7 +20,6 @@ if [ -z "${URL}" -o -z "${USERNAME}" -o -z "${PASSWORD}" -o -z "${AGENTNAME}" ];
   USAGE
 fi
 
-
 #Install java
 type java &>/dev/null
 if [ $? -ne 0 ]; then
@@ -28,8 +27,13 @@ if [ $? -ne 0 ]; then
   exit 2
 fi
 #Download cli jar file
+curl -f -s -o /tmp/cli.jar ${URL}/jnlpJars/jenkins-cli.jar
+if [ $? -ne 0 ]; then
+  echo -e "e\[1;31m Unable to download Jenkins CLI"
+  exit 2
+fi
+
 #Create Agent with CLI
 #Setup xml file with agent name
 #Configure Agent with CLI
 #Setup Jenkins startup script
-
