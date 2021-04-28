96471cec4619508a6cab02257ac7f544eb94ab83
ens36256 <ens36256@adobe.com>
2021-04-28 19:58:18 +0100

    slave setup.sh

---
+++ jenkins-slave-setup/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+## Arguments needed are
+# 1. IP adderss of Jenkins
+# 2. Username
+# 3. Password
+
+USAGE() {
+  echo -e "\e[1;31, Invalid Inputs\e[0m"
+  echo -e "\e1m $0 URL(http://IP:8080) USERNAME PASSWORD AGENTNAME \e[0m"
+  exit 1
+}
+
+URL=$1
+USERNAME=$2
+PASSWORD=$3
+AGENTNAME=$4
+
+if [ -z "${URL}" -o -z "${USERNAME}" -o -z "${PASSWORD}" -o -z "${AGENTNAME}" ]; then
+  USAGE
+fi
+
+echo All Good
+
+#Install java
+#Download cli jar file
+#Create Agent with CLI
+#Setup xml file with agent name
+#Configure Agent with CLI
+#Setup Jenkins startup script
+
